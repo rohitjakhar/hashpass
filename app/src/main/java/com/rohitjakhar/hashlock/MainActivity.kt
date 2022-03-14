f9348e90ce1f225e03f38dc58fb231b0580406c0
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-14 21:14:52 +0530

    Create Empty Project of HashLock

---
+++ app/src/main/java/com/rohitjakhar/hashlock/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rohitjakhar.hashlock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
