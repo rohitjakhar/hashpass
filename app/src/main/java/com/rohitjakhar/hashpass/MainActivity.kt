49e0d02f6193e73cb651c30b5811e4a51bd3b107
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-18 12:25:37 +0530

    Complete Navigation Flow.

--- app/src/main/java/com/rohitjakhar/hashpass/MainActivity.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.rohitjakhar.hashpass
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.rohitjakhar.hashpass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        val navController = fragmentManager.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,
+                R.id.nav_add_password,
+                R.id.nav_generate_password,
+                R.id.nav_setting
+            )
+        )
+
+        binding.bottomNavBar.setupWithNavController(navController)
     }
 }
