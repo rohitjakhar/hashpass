c891d81e18111f49e027ab19b775ace7e8df9331
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-20 12:05:49 +0530

    Update Model. Create UI for Home Screen.

--- app/src/main/java/com/rohitjakhar/hashpass/MyApp.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/MyApp.kt
@@ -2,6 +2,20 @@ package com.rohitjakhar.hashpass
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.hasura.sdk.Hasura
+import io.hasura.sdk.ProjectConfig
 
 @HiltAndroidApp
-class MyApp : Application()
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val hasuraConfig = ProjectConfig.Builder()
+            .setProjectName("hashpass")
+            .build()
+        Hasura.setProjectConfig(hasuraConfig)
+            .enableLogs()
+            .initialise(this)
+        val user = Hasura.getClient().user
+    }
+}
--- app/src/main/java/com/rohitjakhar/hashpass/data/model/PasswordModel.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/data/model/PasswordModel.kt
@@ -2,11 +2,19 @@ package com.rohitjakhar.hashpass.data.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.*
 
 @Parcelize
 data class PasswordModel(
-    val email: String,
+    val email: String? = null,
     val passwordHash: String,
     val createdAt: Long,
-    val categoryModel: CategoryModel
+    val url: String,
+    val uuid: UUID,
+    val userName: String? = null,
+    val title: String,
+    val description: String? = null,
+    val securityQuestion: String? = null,
+    val securityAnswer: String? = null,
+    val remarks: String? = null
 ) : Parcelable
