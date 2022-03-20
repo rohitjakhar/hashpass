46f40b6def9d9eeaa3cd8cd1de8d5f532885dea3
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-20 15:50:48 +0530

    Implement Graphql

---
+++ app/src/main/java/com/rohitjakhar/hashpass/di/ApolloModel.kt
@@ -0,0 +1,27 @@
+package com.rohitjakhar.hashpass.di
+
+import com.rohitjakhar.hashpass.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApolloModel {
+    @Provides
+    @Named("graphQLClient")
+    fun providesOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val builder = original.newBuilder().method(original.method, original.body)
+                builder.header("X-Hasura-Admin-Secret", BuildConfig.HASURA_KEY)
+                chain.proceed(builder.build())
+            }
+            .build()
+}
