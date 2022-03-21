51586fae5ed26a100705a8f90d647150e4b20238
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-21 10:28:15 +0530

    Update Datastore for more field eg name, email, image, id

--- app/src/main/java/com/rohitjakhar/hashpass/data/local/HashPassPreferenceDataStore.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/data/local/HashPassPreferenceDataStore.kt
@@ -4,6 +4,14 @@ import kotlinx.coroutines.flow.Flow
 
 interface HashPassPreferenceDataStore {
     val isLogin: Flow<Boolean>
+    val userName: Flow<String>
+    val userEmail: Flow<String>
+    val userImage: Flow<String>
+    val userId: Flow<String>
 
     suspend fun changeLogin(isLogin: Boolean)
+    suspend fun updateName(userName: String)
+    suspend fun updateEmail(email: String)
+    suspend fun updateImage(imageLink: String)
+    suspend fun updateId(userId: String)
 }
--- app/src/main/java/com/rohitjakhar/hashpass/data/local/PreferenceDataImpl.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/data/local/PreferenceDataImpl.kt
@@ -4,6 +4,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -16,13 +17,61 @@ class PreferenceDataImpl @Inject constructor(
             it[PREF_IS_LOGIN] ?: false
         }
 
+    override val userEmail: Flow<String>
+        get() = dataStore.data.map {
+            it[PREF_USER_EMAIL] ?: ""
+        }
+
+    override val userId: Flow<String>
+        get() = dataStore.data.map {
+            it[PREF_USER_ID] ?: ""
+        }
+
+    override val userImage: Flow<String>
+        get() = dataStore.data.map {
+            it[PREF_USER_IMAGE] ?: ""
+        }
+
+    override val userName: Flow<String>
+        get() = dataStore.data.map {
+            it[PREF_USER_NAME] ?: ""
+        }
+
     override suspend fun changeLogin(isLogin: Boolean) {
         dataStore.edit {
             it[PREF_IS_LOGIN] = isLogin
         }
     }
 
+    override suspend fun updateName(userName: String) {
+        dataStore.edit {
+            it[PREF_USER_NAME] = userName
+        }
+    }
+
+    override suspend fun updateEmail(email: String) {
+        dataStore.edit {
+            it[PREF_USER_EMAIL] = email
+        }
+    }
+
+    override suspend fun updateImage(imageLink: String) {
+        dataStore.edit {
+            it[PREF_USER_IMAGE] = imageLink
+        }
+    }
+
+    override suspend fun updateId(userId: String) {
+        dataStore.edit {
+            it[PREF_USER_ID] = userId
+        }
+    }
+
     companion object {
         private val PREF_IS_LOGIN = booleanPreferencesKey("isLogin")
+        private val PREF_USER_NAME = stringPreferencesKey("user_name")
+        private val PREF_USER_EMAIL = stringPreferencesKey("user_email")
+        private val PREF_USER_ID = stringPreferencesKey("user_id")
+        private val PREF_USER_IMAGE = stringPreferencesKey("user_image")
     }
 }
