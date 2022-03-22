3d309d93ac7d6a82097309a6b56e3596fef33354
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-22 13:42:44 +0530

    Add Icon. Implement In App Update.

--- app/src/main/java/com/rohitjakhar/hashpass/SplashScreen.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/SplashScreen.kt
@@ -4,15 +4,58 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.rohitjakhar.hashpass.presention.AuthActivity
 
+const val UPDATE_REQUEST_CODE = 524
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultLauncher ->
+            if (resultLauncher.resultCode == RESULT_OK) {
+            }
+        }
+    private val appUpdateManager: AppUpdateManager by lazy {
+        AppUpdateManagerFactory.create(applicationContext)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkUpdate()
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+    private fun checkUpdate() {
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+        appUpdateInfoTask.addOnSuccessListener {
+            if (it.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                appUpdateManager.startUpdateFlowForResult(
+                    it,
+                    this,
+                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
+                        .setAllowAssetPackDeletion(true)
+                        .build(),
+                    UPDATE_REQUEST_CODE
+                )
+                resultLauncher.launch(intent)
+            } else {
+                checkUserLogin()
+            }
+        }
+            .addOnFailureListener {
+                checkUserLogin()
+            }
+    }
+
+    private fun checkUserLogin() {
         if (Firebase.auth.currentUser != null) {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
@@ -20,6 +63,5 @@ class SplashScreen : AppCompatActivity() {
             startActivity(Intent(this, AuthActivity::class.java))
             finish()
         }
-        setContentView(R.layout.activity_splash_screen)
     }
 }
