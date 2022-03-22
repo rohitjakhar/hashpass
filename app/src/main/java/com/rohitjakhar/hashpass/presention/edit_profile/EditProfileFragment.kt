69da35638c153991b13e4b1ecddc5c0bcfb87b4b
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-22 13:15:55 +0530

    Add buisness logic for edit profile

--- app/src/main/java/com/rohitjakhar/hashpass/presention/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/presention/edit_profile/EditProfileFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.rohitjakhar.hashpass.databinding.FragmentEditProfileBinding
+import com.rohitjakhar.hashpass.utils.loadingView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class EditProfileFragment : Fragment() {
@@ -15,6 +18,7 @@ class EditProfileFragment : Fragment() {
     private var _binding: FragmentEditProfileBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<EditProfileVM>()
+    private val loadingView by lazy { requireActivity().loadingView(cancelable = false) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +26,26 @@ class EditProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        viewModel.getUserDetails()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        collectUserDetails()
+        initClick()
+    }
+
+    private fun initClick() = binding.apply {
+    }
+
+    private fun collectUserDetails() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.userDetailsState.collectLatest {
+                if (it != null) {
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/rohitjakhar/hashpass/presention/edit_profile/EditProfileVM.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/presention/edit_profile/EditProfileVM.kt
@@ -1,13 +1,35 @@
 package com.rohitjakhar.hashpass.presention.edit_profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rohitjakhar.hashpass.data.model.UserDetailsModel
 import com.rohitjakhar.hashpass.data.remote.LoginRepo
+import com.rohitjakhar.hashpass.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class EditProfileVM @Inject constructor(
     private val loginRepo: LoginRepo
 ) : ViewModel() {
+    var userDetailsState = MutableStateFlow<UserDetailsModel?>(null)
+        private set
+    var updateState = MutableStateFlow<Resource<Unit>>(Resource.Loading())
+        private set
 
+    fun getUserDetails() {
+        viewModelScope.launch(IO) {
+            userDetailsState.emit(loginRepo.getUserDetails())
+        }
+    }
+
+    fun updateProfile(userDetailsModel: UserDetailsModel) {
+        viewModelScope.launch(IO) {
+            updateState.emit(Resource.Loading())
+            updateState.emit(loginRepo.updateUser(userDetailsModel))
+        }
+    }
 }
