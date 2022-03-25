2aeede508c018de0e7c68a94ff3a9334beca1dc0
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-25 16:10:06 +0530

    Load User Details at Edit Profile Screen.

--- app/src/main/java/com/rohitjakhar/hashpass/presention/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/presention/edit_profile/EditProfileFragment.kt
@@ -7,6 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.avatarfirst.avatargenlib.AvatarConstants
+import com.avatarfirst.avatargenlib.AvatarGenerator
+import com.rohitjakhar.hashpass.data.model.UserDetailsModel
 import com.rohitjakhar.hashpass.databinding.FragmentEditProfileBinding
 import com.rohitjakhar.hashpass.utils.loadingView
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,9 +48,37 @@ class EditProfileFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             viewModel.userDetailsState.collectLatest {
                 if (it != null) {
+                    updateProfile(it)
                 }
             }
         }
+    }
+
+    private fun updateProfile(userDetailsModel: UserDetailsModel) = binding.apply {
+        ivUploadPhoto.load(userDetailsModel.userImage) {
+            transformations(CircleCropTransformation())
+            error(
+                AvatarGenerator.avatarImage(
+                    requireContext(),
+                    AvatarConstants.COLOR400,
+                    shape = AvatarConstants.CIRCLE,
+                    userDetailsModel.name,
+                    colorModel = AvatarConstants.COLOR400
+                )
+            )
+            placeholder(
+                AvatarGenerator.avatarImage(
+                    requireContext(),
+                    AvatarConstants.COLOR400,
+                    shape = AvatarConstants.CIRCLE,
+                    userDetailsModel.name,
+                    colorModel = AvatarConstants.COLOR400
+                )
+            )
+        }
+
+        inputLayoutEmail.editText?.setText(userDetailsModel.email)
+        inputLayoutName.editText?.setText(userDetailsModel.name)
     }
 
     override fun onDestroy() {
