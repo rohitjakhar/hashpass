13727e0ab20ad275424113e15b0d1f63bb42e99b
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-31 00:08:55 +0530

    Implement share button with share password details.

--- app/src/main/java/com/rohitjakhar/hashpass/presention/password_details/PasswordDetailsFragment.kt
+++ app/src/main/java/com/rohitjakhar/hashpass/presention/password_details/PasswordDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.rohitjakhar.hashpass.presention.password_details
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.rohitjakhar.hashpass.R
+import com.rohitjakhar.hashpass.data.model.PasswordModel
 import com.rohitjakhar.hashpass.databinding.FragmentPasswordDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -37,6 +39,40 @@ class PasswordDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        initClick()
+    }
+
+    private fun buildPasswordShare(passwordModel: PasswordModel): String {
+        val passwordBuilder = StringBuilder()
+        with(passwordModel) {
+            passwordBuilder.append("Title: $title")
+            passwordBuilder.append("\nUrl: $url")
+            passwordBuilder.append("\nPassword: $passwordHash")
+            if (email != null && email.isNotEmpty()) passwordBuilder.append("\nEmail: $email")
+            if (userName != null && userName.isNotEmpty()) passwordBuilder.append("\nUser Name: $userName")
+            if (description != null && description.isNotEmpty()) passwordBuilder.append("\nDescription: $description")
+            if (securityQuestion != null && securityQuestion.isNotEmpty()) passwordBuilder.append("\nSecurity Question: $securityQuestion")
+            if (securityAnswer != null && securityAnswer.isNotEmpty()) passwordBuilder.append("\nSecurity Answer: $securityAnswer")
+            if (remarks != null && remarks.isNotEmpty()) passwordBuilder.append("\nRemarks: $remarks")
+            passwordBuilder.append("\nDownload HashPass for saving password at free!\nLink:- https://play.google.com/store/apps/details?id=com.rohit.hashpass")
+        }
+        return passwordBuilder.toString()
+    }
+
+    private fun initClick() = binding.apply {
+        val passwordDetails = navArgs.passwordDetails
+        floatingShare.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    buildPasswordShare(passwordDetails)
+                )
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, "Share Password")
+            requireContext().startActivity(shareIntent)
+        }
     }
 
     private fun initView() = binding.apply {
